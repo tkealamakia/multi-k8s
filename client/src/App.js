ce0f05045f71efd511f39b41b53cd9b133eb37b7
tkealamakia <tkealamakia@overstock.com>
2020-01-04 10:29:11 -0700

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,15 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <h1 className="App-title">Fib Calculator version 2</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
+          </div>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
           </div>
         </div>
       </Router>
