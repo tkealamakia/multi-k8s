6e3bd57390bd7e1f87454ea8800934637fe2e96e
tkealamakia <tkealamakia@overstock.com>
2020-01-04 12:57:17 -0700

    added encrypted service account file

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+//pgClient
+//  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+//  .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
@@ -40,6 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
+  const xvalues = await pgClient.query("CREATE TABLE IF NOT EXISTS values(number INT)");
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
